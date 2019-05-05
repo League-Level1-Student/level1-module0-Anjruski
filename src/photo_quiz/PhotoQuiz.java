1454fd652a531630f97eacf9653c53083524273d
Anjruski <league@iMac-7.attlocal.net>
2019-05-05 14:26:01 -0700

    :sleeping: , I'm tired

--- src/photo_quiz/PhotoQuiz.java
+++ src/photo_quiz/PhotoQuiz.java
@@ -13,6 +13,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class PhotoQuiz {
 
@@ -22,7 +23,8 @@ public class PhotoQuiz {
 		quizWindow.setVisible(true);
 		// This will make sure the program exits when you close the window
 		quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+JPanel quizPanel = new JPanel();
+quizWindow.add(quizPanel);
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address”)
@@ -33,31 +35,42 @@ String url = "https://upload.wikimedia.org/wikipedia/commons/4/4f/Trader_Joe%27s
 		// 3. use the "createImage()" method below to initialize your Component
 image=createImage(url);
 		// 4. add the image to the quiz window
-quizWindow.add(image);
+quizPanel.add(image);
 		// 5. call the pack() method on the quiz window
 quizWindow.pack();
 		// 6. ask a question that relates to the image
-//String answer = JOptionPane.showInputDialog("What is this?");
+String answer = JOptionPane.showInputDialog("What is this?");
 		// 7. print "CORRECT" if the user gave the right answer
-//if (answer.equals("Ice cream sandwitch")) {
-	//JOptionPane.showMessageDialog(null, "CORRECT");
-//}
+if (answer.equalsIgnoreCase("Ice cream sandwitch")) {
+	JOptionPane.showMessageDialog(null, "Correct!");
+}
 		// 8. print "INCORRECT" if the answer is wrong
-//else {JOptionPane.showMessageDialog(null, "INCORRECT");}
+else {JOptionPane.showMessageDialog(null, "Incorrect");}
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+quizPanel
+.remove(image);
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
-		// 11. add the second image to the quiz window
-
-		// 12. pack the quiz window
-
+String url2 = "https://vignette.wikia.nocookie.net/jackinthebox/images/9/92/MonsterTaco.png/revision/latest?cb=20140626151552";
+Component image2;
+image2=createImage(url2);
+quizPanel.add(image2);
+// 11. add the second image to the quiz window
+String url3 = "https://images-na.ssl-images-amazon.com/images/I/312cIkdY6SL._SR600%2C315_PIWhiteStrip%2CBottomLeft%2C0%2C35_SCLZZZZZZZ_.jpg";
+Component image3;
+image3=createImage(url3);
+quizPanel.add(image3);
+// 12. pack the quiz window
+quizWindow.pack();
 		// 13. ask another question
-
+String Answer2 = JOptionPane.showInputDialog("What does this mean?");
 		// 14+ check answer, say if correct or incorrect, etc.
-
+if (Answer2 .equalsIgnoreCase("Taco Bell")) {
+	JOptionPane.showMessageDialog(null, "Correct!");
+}
+else {JOptionPane.showMessageDialog(null, "Incorrect");}
+	return;
 	}
 
 	private Component createImage(String imageUrl) throws MalformedURLException {
