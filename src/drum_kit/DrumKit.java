7d50646e8d1c5b978e93d04f12c03cac9291a316
League Student <league@iMac-8.attlocal.net>
2019-05-12 14:33:05 -0700

    whoops

--- src/drum_kit/DrumKit.java
+++ src/drum_kit/DrumKit.java
@@ -5,6 +5,7 @@ package drum_kit;
  */
 
 import java.applet.AudioClip;
+import java.awt.Component;
 import java.awt.GridLayout;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -25,30 +26,32 @@ public class DrumKit implements MouseListener {
 	public void run() throws MalformedURLException {
 
 		// 1. Make a JFrame variable and initialize it using "new JFrame()"
-
+JFrame JF = new JFrame();
 		// 2. Make the frame visible and
 		// set its default close operation to JFrame.EXIT_ON_CLOSE
-
+JF.setVisible(true);
+JF.setDefaultCloseOperation(JF.EXIT_ON_CLOSE);
 		// 3. Set the size of the frame
-
-		// 4. Set the title of the frame
-
+JF.setSize(200,200 );
+	// 4. Set the title of the frame
+String Title = "[insert title here]";
+JF.setTitle(Title);
 		// 5. Make a JPanel variable and initialize it using "new JPanel().
-
+JPanel JP = new JPanel();
 		// 6. Add the panel to the frame. (The panel is invisible.)
-
+JF.add(JP);
 		// 7. Download an image of a drum from the Internet. Drop it into your
 		// Eclipse project under "default package".
 
 		// 8. Put the name of your image file in a String variable.
-
+ String Image = "Drummi Boi";
 		// 9. Edit the next line to use your String variable
 		// drumLabelWithImage = createLabelImage(drumImageString);
-
+drumLabelWithImage = createLabelImage(Image);
 		// 10. Add the image to the panel
-
+JP.add(drumLabelWithImage);
 		// 11. Set the layout of the panel to "new GridLayout()"
-
+JP.setLayout(new GridLayout());
 		// 12. call the pack() method on the frame. Run your program. Do you see
 		// your drum image?
 
