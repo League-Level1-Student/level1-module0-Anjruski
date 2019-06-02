2955d59d66a82860caa36ef072e841f0b81f3b98
League Student <league@iMac-47.attlocal.net>
2019-06-02 14:25:11 -0700

    :smile:
    :relieved:

---
+++ src/FortuneRUNer.java
@@ -0,0 +1,7 @@
+public class FortuneRUNer {
+  public static void main(String[] args) {
+	  System.out.println("Button clicked");
+FortuneiBoi fortune = new FortuneiBoi();
+fortune.showButton();
+  }
+}
--- src/FortuneiBoi.java
+++ src/FortuneiBoi.java
@@ -1,49 +1,39 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
-public class FortuneiBoi {
-public static void main(String[] args) {
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class FortuneiBoi implements ActionListener {
+	public void showButton() {
+		System.out.println("Button clicked");
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		JButton button = new JButton();
+		frame.add(button);
+		frame.pack();
+		button.addActionListener(this);
+		button.setVisible(true);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		int rand = new Random().nextInt(5);
+		if (rand == 1) {
+			System.out.println("Pick another fortune cookie");
+		} else if (rand == 2) {
+			System.out.println("This is filled with things you can't pronounce!");
+		} else if (rand == 3) {
+			System.out.println("life is like a race, every second gets you closer to the finish line.");
+		} else if (rand == 4) {
+			System.out.println("This is only paper, not a crystal ball.");
+		} else {
+			System.out.println("I am not a cookie. ( ͡° ͜ʖ ͡°)");
+		}
+		System.out.println();
+		// TODO Auto-generated method stub
+
+	}
 }
